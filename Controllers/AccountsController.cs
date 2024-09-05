93167f939f4291064b3925ef44cacd00a598bc9a
Ibaa <ebaahmad12@gmail.com>
2024-09-05 13:44:14 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,58 @@
+﻿using IdentityApp.Data;
+using IdentityApp.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountsController(ApplicationDbContext context , UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) {
+            this.context = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> Register(RegisterViewModel model)
+        {
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = model.Name,
+                Email = model.Email,
+                PasswordHash = model.Password,
+
+            };
+           var result=await  userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+
+            }
+            return View(model);
+        }
+        public IActionResult Login()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email , model.Password, model.RememberMe, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            return View(model);
+
+        }
+    }
+}
---
+++ Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(40)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(40)]
+        [Compare(nameof(Password))]
+        public string ConfirmPasseord { get; set; }
+    }
+}
